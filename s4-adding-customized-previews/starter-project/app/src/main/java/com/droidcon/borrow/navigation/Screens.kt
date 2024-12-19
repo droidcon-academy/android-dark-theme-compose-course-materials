5c7b7285a0370a6dde80b0ec5b6055b106b6d3c5
Musab Nasredlin <musabagab2010@gmail.com>
2024-12-19 16:20:57 +0400

    Updated projects

--- s4-adding-customized-previews/starter-project/app/src/main/java/com/droidcon/borrow/navigation/Screens.kt
+++
@@ -1,6 +0,0 @@
-package com.droidcon.borrow.navigation
-
-sealed class Screens(val route: String) {
-  object ListScreen : Screens("listScreen")
-  object ItemInputScreen : Screens("itemInputScreen")
-}
